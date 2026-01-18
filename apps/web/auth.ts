31f6b20863049cef74f2e8fd2d02dd253fcb2478
Shivam Gupta <shivamgupta02005@gmail.com>
2026-01-18 22:30:00 +0530

    clean up next-auth code from web app

--- apps/web/auth.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth, { type NextAuthResult } from 'next-auth';
-import { authConfig } from './config/auth.config';
-
-const result = NextAuth(authConfig);
-
-export const handlers: NextAuthResult['handlers'] = result.handlers;
-export const auth: NextAuthResult['auth'] = result.auth;
-export const signIn: NextAuthResult['signIn'] = result.signIn;
-export const signOut: NextAuthResult['signOut'] = result.signOut;
